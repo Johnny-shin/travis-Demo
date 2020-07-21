0452394fc5fddd3cb0781d973aeec50676f25e01
Johnny <superstar41668087@gmail.com>
2020-07-21 16:45:35 -0400

    built public React app including build file

---
+++ build/precache-manifest.bdebfc95f16016ae3be5fedbee2e5ddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d2001b0d5bcc0caba5601f3febe596",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0b378186c6bccc2742",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbfe29c6f7df2ae23fc5",
+    "url": "/static/js/2.a2c26cd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2c26cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0b378186c6bccc2742",
+    "url": "/static/js/main.9f82131b.chunk.js"
+  },
+  {
+    "revision": "86a436665469bd4a8dbd",
+    "url": "/static/js/runtime-main.2d47aea1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
